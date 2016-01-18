80414a2975ba90aca68dbc8c97bbf411db27b62a
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2016-01-18 15:04:03 +0000

    Add write(T model) method without indentation parameter

--- HtmlFlow/src/main/java/htmlflow/HtmlWriter.java
+++ HtmlFlow/src/main/java/htmlflow/HtmlWriter.java
@@ -5,7 +5,16 @@ import java.io.PrintStream;
 public interface HtmlWriter<T>{
 	/**
 	 * Writes into an internal PrintStream the HTML content
-	 * if this element.
+	 * of this element with initial indentation of zero.
+	 *
+	 * @param model An optional object model that could be bind to this element.
+	 */
+	default void write(T model) {
+		write(0, model);
+	}
+	/**
+	 * Writes into an internal PrintStream the HTML content
+	 * of this element.
 	 * 
 	 * @param depth The number of tabs indentation.
 	 * @param model An optional object model that could be bind to this element. 
