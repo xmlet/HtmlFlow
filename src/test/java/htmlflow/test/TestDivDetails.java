1e682ffcc85b23eca1e2e211fdf19c1ba1708c58
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2016-01-25 16:58:55 +0000

    Add license to package htmlflow.test files

--- src/test/java/htmlflow/test/TestDivDetails.java
+++ src/test/java/htmlflow/test/TestDivDetails.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2016, Miguel Gamboa
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package htmlflow.test;
 
 import htmlflow.HtmlView;
@@ -18,7 +34,8 @@ import java.io.*;
 import java.util.Arrays;
 
 /**
- * Created by mcarvalho on 18-01-2016.
+ * @author Miguel Gamboa
+ * Created on 18-01-2016.
  */
 public class TestDivDetails {
     @Test
