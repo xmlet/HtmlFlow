50c6c875315b6015ca9e31318d8563048615ebb2
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2016-01-25 17:06:07 +0000

    Add license to package htmlflow.elements files

--- src/main/java/htmlflow/elements/HtmlBody.java
+++ src/main/java/htmlflow/elements/HtmlBody.java
@@ -1,9 +1,29 @@
+/*
+ * Copyright (c) 2016, Miguel Gamboa
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package htmlflow.elements;
 
 import htmlflow.HtmlWriterComposite;
 import htmlflow.ModelBinder;
 import htmlflow.TextNode;
 
+/**
+ * @author Miguel Gamboa
+ *         created on 29-03-2012
+ */
 public class HtmlBody<T> extends HtmlWriterComposite<T, HtmlBody<T>>{	
 	
 	public HtmlBody<T> heading(int level, String msg){addChild(new HtmlHeading<T>(level)).text(msg); return this;}
--- src/main/java/htmlflow/elements/HtmlDiv.java
+++ src/main/java/htmlflow/elements/HtmlDiv.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2016, Miguel Gamboa
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package htmlflow.elements;
 
 import htmlflow.HtmlWriterComposite;
@@ -5,7 +21,8 @@ import htmlflow.ModelBinder;
 import htmlflow.TextNode;
 
 /**
- * @uml.dependency   supplier="htmlflow.ModelBinder"
+ * @author Miguel Gamboa
+ *         created on 29-03-2012
  */
 public class HtmlDiv<T> extends HtmlWriterComposite<T, HtmlDiv<T>> {
 	
