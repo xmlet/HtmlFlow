6dfd1a2f10b4d4dcaa3698aaa4e907368ece2bbc
Lukas Havemann <havemann.lukas@googlemail.com>
2017-12-06 18:42:56 +0100

    Add StyleBlock to head

--- src/main/java/htmlflow/elements/HtmlHead.java
+++ src/main/java/htmlflow/elements/HtmlHead.java
@@ -34,6 +34,7 @@ public class HtmlHead<T> extends HtmlWriterComposite<T, HtmlHead<T>>{
     public HtmlHead<T> title(String msg){addChild(new HtmlTitle<T>()).text(msg);return this;}
     public HtmlHead<T> scriptLink(String src){addChild(new HtmlScriptLink<T>(src));return this;}
     public HtmlScriptBlock<T> scriptBlock(){return addChild(new HtmlScriptBlock<T>());}
+    public HtmlStyleBlock<T> styleBlock(){return addChild(new HtmlStyleBlock<T>());}
     public HtmlHead<T> linkCss(String href){addChild(new HtmlLinkCss<T>(href));return this;}
 
     @Override
