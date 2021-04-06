17391c7499d768125c160ebc80809f513f74d023
Miguel Gamboa <miguelgamboa@outlook.com>
2021-04-06 13:41:41 +0100

    Modify unit test TestPartialsToPrintStream to manage large data and incur in OutOfMemory. Update pom.xml to run test with max heap size of 32 Mb

--- src/test/java/htmlflow/test/TestPartialsToPrintStream.java
+++ src/test/java/htmlflow/test/TestPartialsToPrintStream.java
@@ -1,21 +1,29 @@
 package htmlflow.test;
 
 import htmlflow.DynamicHtml;
+import htmlflow.HtmlView;
+import htmlflow.HtmlVisitorCache;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
 import java.io.PrintStream;
+import java.lang.reflect.Field;
 import java.util.Iterator;
-import java.util.regex.Pattern;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-import static htmlflow.test.Utils.NEWLINE;
-import static htmlflow.test.Utils.loadLines;
+import static java.io.OutputStream.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
 public class TestPartialsToPrintStream {
-    @Test
+
+    final static int MB = 1024*1024;
+
+     @Test
     public void testAddPartialAndCheckParentPrintStream() {
         /**
          * Arrange
@@ -34,12 +42,54 @@ public class TestPartialsToPrintStream {
         /**
          * Assert
          */
-        Iterator<String> actual = NEWLINE
+        Iterator<String> actual = Utils
+            .NEWLINE
             .splitAsStream(mem.toString())
             .iterator();
-        loadLines("testAddPartialAndCheckParentPrintStream.html")
+        Utils
+            .loadLines("testAddPartialAndCheckParentPrintStream.html")
             .forEach(expected -> assertEquals(expected, actual.next()));
         assertFalse(actual.hasNext());
+    }
+
+    /**
+     * Discards all bytes through the PrintStream.
+     * Testing with large data model to check that partial view is not internally buffering strings and
+     * is not incurring in OutOfMemoryError.
+     */
+    @Test
+    public void testAddPartialWithLargeData() throws NoSuchFieldException, IllegalAccessException {
+        /**
+         * Arrange
+         */
+        Stream<Penguin> penguins = IntStream
+            .range(0, 128)
+            .mapToObj(n -> new Penguin("Albert" + n, getSaltString(1 * MB)));
+        /**
+         * Act
+         */
+        // Discards all bytes through the PrintStream.
+        DynamicHtml<Stream<Penguin>> view = DynamicHtml.view(new PrintStream(nullOutputStream()), this::template);
+        view.write(penguins);
+        /**
+         * Assert
+         */
+        HtmlVisitorCache visitor = view.getVisitor();
+        Field blocksField = visitor.getClass().getSuperclass().getDeclaredField("cacheBlocksList");
+        blocksField.setAccessible(true);
+        List blocks = (List) blocksField.get(visitor);
+        assertEquals(2, blocks.size());
+    }
+
+    static  String getSaltString(int length) {
+        char[] SALTCHARS= "ABCDEFGHIJKLMNOPQRSTUVWXYZ ".toCharArray();
+        StringBuilder salt = new StringBuilder();
+        Random rnd = new Random();
+        while (salt.length() < length) { // length of the random string.
+            int index = rnd.nextInt(SALTCHARS.length);
+            salt.append(SALTCHARS[index]);
+        }
+        return salt.toString();
     }
 
     static class Penguin {
@@ -63,16 +113,17 @@ public class TestPartialsToPrintStream {
                     .div().attrClass("local-storage").attrStyle("display: none;")
                         .dynamic(localStorageDiv ->
                             model.forEachOrdered(penguin ->
-                                view.addPartial(DynamicHtml.view(this::penguinPartialTemplate), penguin)))
+                                view.addPartial(penguimPartialView, penguin)))
                     .__()
                 .__() //body
               .__(); //html
     }
-    private  void penguinPartialTemplate(DynamicHtml<Penguin> view, Penguin penguin){
+
+    private final HtmlView<Penguin> penguimPartialView = DynamicHtml.view((view, penguin) -> {
         view
             .div()
                 .attrId("data-penguin-" + penguin.getName())
                 .attrStyle("display: none;")
                 .addAttr("data-dna",penguin.getDnaCode());
-    }
+    });
 }
