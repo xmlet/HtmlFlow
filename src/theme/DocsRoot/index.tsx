96f5f71f9f5e4d013af4e10f2387dab9f8cf3340
Arthur Oliveira <arthurcnoliveira@gmail.com>
2026-02-17 19:25:14 +0000

    React components addition

---
+++ src/theme/DocsRoot/index.tsx
@@ -0,0 +1,15 @@
+import React, { type ReactNode } from 'react';
+import clsx from 'clsx';
+import { ThemeClassNames, HtmlClassNameProvider } from '@docusaurus/theme-common';
+import renderRoutes from '@docusaurus/renderRoutes';
+import Layout from '@theme/Layout';
+
+import type { Props } from '@theme/DocVersionRoot';
+
+export default function DocsRoot(props: Props): ReactNode {
+  return (
+    <HtmlClassNameProvider className={clsx(ThemeClassNames.wrapper.docsPages, 'test')}>
+      <Layout>{renderRoutes(props.route.routes!)}</Layout>
+    </HtmlClassNameProvider>
+  );
+}
